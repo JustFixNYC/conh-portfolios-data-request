1ddfd01ab85c4c61c45ecaf0a90988639425a882
Atul Varma <varmaa@gmail.com>
2019-12-13 18:49:47 -0500

    Add a bbl function.

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,12 @@ struct CONHRecord {
     lot: u16,
 }
 
+impl CONHRecord {
+    fn bbl(self) -> String {
+        format!("{}{:05}{:04}", self.boro, self.block, self.lot)
+    }
+}
+
 fn iter_conh_records() -> impl Iterator<Item = CONHRecord> {
     let file = File::open("./data/Certification_of_No_Harassment__CONH__Pilot_Building_List.csv").unwrap();
     let rdr = csv::Reader::from_reader(file);
@@ -23,7 +29,7 @@ fn iter_conh_records() -> impl Iterator<Item = CONHRecord> {
 fn main() {
     for (i, rec) in iter_conh_records().enumerate() {
         if i > 10 { break; }
-        println!("Row #{} {:?}", i + 1, rec);
+        println!("Row #{} {}", i + 1, rec.bbl());
     }
 }
 
@@ -34,4 +40,10 @@ fn test_csv_is_parseable() {
         if rec.boro == 1 { found = true; }
     }
     assert_eq!(found, true);
+}
+
+#[test]
+fn test_bbl_works() {
+    let rec = CONHRecord { boro: 1, block: 5099, lot: 39 };
+    assert_eq!(rec.bbl(), "1050990039");
 }
