4aae81a608ee53b6bcd8aaa429d6f188836e1af6
Atul Varma <varmaa@gmail.com>
2019-12-13 19:11:17 -0500

    Download WoW info.

--- src/main.rs
+++ src/main.rs
@@ -14,9 +14,19 @@ struct CONHRecord {
 }
 
 impl CONHRecord {
-    fn bbl(self) -> String {
+    fn bbl(&self) -> String {
         format!("{}{:05}{:04}", self.boro, self.block, self.lot)
     }
+
+    fn wow_address_api_url(&self) -> String {
+        format!("https://whoownswhat.justfix.nyc/api/address?block={:05}&lot={:04}&borough={}", self.block, self.lot, self.boro)
+    }
+}
+
+fn download_wow_info(rec: &CONHRecord) -> String {
+    let url = rec.wow_address_api_url();
+    let result = reqwest::get(url.as_str()).unwrap().text().unwrap();
+    result
 }
 
 fn iter_conh_records() -> impl Iterator<Item = CONHRecord> {
@@ -28,8 +38,9 @@ fn iter_conh_records() -> impl Iterator<Item = CONHRecord> {
 
 fn main() {
     for (i, rec) in iter_conh_records().enumerate() {
-        if i > 10 { break; }
+        if i > 1 { break; }
         println!("Row #{} {}", i + 1, rec.bbl());
+        println!("WOW info: {}", download_wow_info(&rec));
     }
 }
 
@@ -46,4 +57,10 @@ fn test_csv_is_parseable() {
 fn test_bbl_works() {
     let rec = CONHRecord { boro: 1, block: 5099, lot: 39 };
     assert_eq!(rec.bbl(), "1050990039");
+}
+
+#[test]
+fn test_wow_address_api_url_works() {
+    let rec = CONHRecord { boro: 1, block: 5099, lot: 39 };
+    assert_eq!(rec.wow_address_api_url(), "https://whoownswhat.justfix.nyc/api/address?block=05099&lot=0039&borough=1");
 }
